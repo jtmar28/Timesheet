2276ccdcb5bde648724010ae6429551309e51dc6
jtmar28 <justinmartinez0123@gmail.com>
2018-11-19 23:47:18 -0500

    Added function to input current Time into Employee form submit

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+const timeForm = document.querySelector('#timeSheetForm');
+const clockIn = document.querySelector('#clockIn');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const list = document.querySelector('#shiftTable');
+const formTime = document.querySelector('#time');
+getEventListeners();
+
+function getEventListeners(){
+    clockIn.addEventListener('click', employeeTime);
+}
+
+function employeeTime(e){
+    const time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+    let month = time.getMonth() + 1;
+    let day = time.getDate();
+    let year = time.getFullYear();
+    if(hour < 10){
+        hour = `0${hour}`;
+    }
+    if(minute < 10){
+        minute = `0${minute}`;
+    }
+    if(second < 10){
+        second = `0${second}`;
+    }
+    if(month < 10){
+        month = `0${month}`;
+    }
+    if(day < 10){
+        day = `0${day}`;
+    }
+    formTime.value = `${month} ${day}, ${year} ${hour} : ${minute}`;
+    e.preventDefault();
+}
+//Current Date function
 function clock(){
     const clock = new Date();
     let hour = clock.getHours();
@@ -16,6 +55,7 @@ function clock(){
     document.getElementById('minute').innerHTML = minute;
     document.getElementById('second').innerHTML = second;
 }
+//Current Time Function
 function date(){
     const date = new Date();
     let month = date.getMonth() + 1;
