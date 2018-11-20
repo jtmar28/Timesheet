10ee1d6ab3a67dd4b499cbde25e4816daee3c0b8
jtmar28 <justinmartinez0123@gmail.com>
2018-11-20 00:55:49 -0500

    Created Function to add employee to list

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const timeForm = document.querySelector('#timeSheetForm');
+const timeForm = document.querySelector('#timeForm');
+const timeSheetForm = document.querySelector('#timeSheetForm');
 const clockIn = document.querySelector('#clockIn');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const list = document.querySelector('#shiftTable');
 const formTime = document.querySelector('#time');
+// const employDate = document.querySelector('#employeeDate');
+// const employName = document.querySelector('#employeeName');
+
 getEventListeners();
 
 function getEventListeners(){
     clockIn.addEventListener('click', employeeTime);
+    timeSheetForm.addEventListener('submit', addEmployeeTime);
 }
 
 function employeeTime(e){
@@ -34,6 +39,32 @@ function employeeTime(e){
         day = `0${day}`;
     }
     formTime.value = `${month} ${day}, ${year} ${hour} : ${minute}`;
+    e.preventDefault();
+}
+function addEmployeeTime(e){
+    const employ = document.querySelector('#employ');
+    employ.className = 'Selected';
+    const tdEmployeeName = document.createElement('td');
+    const tdCurrentTime = document.createElement('td');
+    const tdCurrentDate = document.createElement('td');
+    const tdDelete = document.createElement('td');
+    const time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+    let month = time.getMonth() + 1;
+    let day = time.getDate();
+    let year = time.getFullYear();
+    tdEmployeeName.appendChild(document.createTextNode(`${firstName.value} ${lastName.value}`));
+    tdCurrentTime.appendChild(document.createTextNode(`${hour} : ${minute}`));
+    tdCurrentDate.appendChild(document.createTextNode(`${month} ${day}, ${year}`));
+    employ.appendChild(tdEmployeeName);
+    employ.appendChild(tdCurrentDate);
+    employ.appendChild(tdCurrentTime);
+    employ.appendChild(tdDelete);
+    const a = document.createElement('a');
+    a.innerHTML = '<i class="fas fa-user-minus"></i>';
+    tdDelete.appendChild(a);
     e.preventDefault();
 }
 //Current Date function
